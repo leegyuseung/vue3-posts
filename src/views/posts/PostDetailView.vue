3102aafcae4839f34970c652f790c8446b9cf216
leegyuseung <lgs4002@naver.com>
2022-11-06 22:34:00 +0900

    페이지 컴포넌트에 Props 전달

--- src/views/posts/PostDetailView.vue
+++ src/views/posts/PostDetailView.vue
@@ -1,8 +1,10 @@
 <template>
   <div>
-    <h2>제목</h2>
-    <p>내용</p>
-    <p class="text-muted">2020-01-01</p>
+    <h2>{{ form.title }}</h2>
+    <p>{{ form.content }}</p>
+    <p class="text-muted">
+      {{ form.createdAt }}
+    </p>
     <hr class="my-4" />
     <div class="row g-2">
       <div class="col-auto">
@@ -28,13 +30,29 @@
 </template>
 
 <script setup>
-import { useRoute, useRouter } from "vue-router";
+import { getPostById } from "@/api/posts";
+import { ref } from "vue";
+import { useRouter } from "vue-router";
 
-const route = useRoute();
+const props = defineProps({
+  id: Number,
+});
+
 const router = useRouter();
-const id = route.params.id;
+/* 
+ref 장점) 객체할당을 할 수 있다. 일관성 단점) .value 로 접근해야한다.
+reactive 장점) .value로 접근할 필요 없다. 단점) 객체할당 불가능 하나씩 입력해주어야한다.
+*/
+const form = ref({});
+
+const fetchPost = () => {
+  const data = getPostById(props.id);
+  form.value = { ...data };
+};
+fetchPost();
 const goListPage = () => router.push({ name: "PostList" });
-const goEditPage = () => router.push({ name: "PostEdit", params: { id } });
+const goEditPage = () =>
+  router.push({ name: "PostEdit", params: { id: props.id } });
 </script>
 
 <style lang="scss" scoped></style>
