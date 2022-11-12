768d1f45a261b7aa1e667558e50742115091db97
leegyuseung <lgs4002@naver.com>
2022-11-12 22:56:15 +0900

    버그 수정(put > patch)

--- src/api/posts.js
+++ src/api/posts.js
@@ -14,8 +14,13 @@ export function createPost(data) {
   return posts.post("", data);
 }
 
+// export function updatePost(id, data) {
+//   return posts.put(`/${id}`, data);
+// }
+
+// 일부 데이터만 수정하려면 put이 아닌 patch 사용해야한다.
 export function updatePost(id, data) {
-  return posts.put(`/${id}`, data);
+  return posts.patch(`/${id}`, data);
 }
 
 export function deletePost(id) {
