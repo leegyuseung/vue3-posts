66b2053fe30784f7fd2eb6ea2da6bbb344325cbe
leegyuseung <lgs4002@naver.com>
2022-11-14 23:29:38 +0900

    Teleport 컴포넌트:Modal만들기

--- src/views/posts/PostListView.vue
+++ src/views/posts/PostListView.vue
@@ -14,6 +14,7 @@
           :content="item.content"
           :created-at="item.createdAt"
           @click="goPage(item.id)"
+          @modal="openModal(item)"
         ></PostItem>
       </template>
     </AppGrid>
@@ -23,6 +24,15 @@
       :page-count="pageCount"
       @page="(page) => (params._page = page)"
     />
+    <Teleport to="#modal">
+      <PostModal
+        v-model="show"
+        :title="modalTitle"
+        :content="modalContent"
+        :createdAt="modalCreatedAt"
+      />
+    </Teleport>
+
     <template v-if="posts && posts.length > 0">
       <hr class="my-5" />
       <AppCard>
@@ -36,6 +46,7 @@
 import AppCard from "@/components/AppCard.vue";
 import AppGrid from "@/components/AppGrid.vue";
 import AppPagination from "@/components/AppPagination.vue";
+import PostModal from "@/components/posts/PostModal.vue";
 import PostFilter from "@/components/posts/PostFilter.vue";
 import PostItem from "@/components/posts/PostItem.vue";
 import PostDetailView from "@/views/posts/PostDetailView.vue";
@@ -89,6 +100,19 @@ const goPage = (id) => {
     // },
     // hash: "#world",
   });
+};
+
+// modal
+const show = ref(false);
+const modalTitle = ref("");
+const modalContent = ref("");
+const modalCreatedAt = ref("");
+
+const openModal = ({ title, content, createdAt }) => {
+  show.value = true;
+  modalTitle.value = title;
+  modalContent.value = content;
+  modalCreatedAt.value = createdAt;
 };
 </script>
 
