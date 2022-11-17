ea4deb6c53f5e2533a372b5c05dda3f7986227c9
leegyuseung <lgs4002@naver.com>
2022-11-17 22:48:42 +0900

    vue3 plugin 만들기

--- src/main.js
+++ src/main.js
@@ -3,17 +3,24 @@ import "bootstrap-icons/font/bootstrap-icons.css";
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "@/router";
-
-createApp(App).use(router).mount("#app");
+import funcPlugins from "./plugins/func";
+import objPlugins from "./plugins/obj";
+import person from "./plugins/person";
+const app = createApp(App);
+app.use(objPlugins, { name: "lee" });
+app.use(funcPlugins);
+app.use(person, { name: "홍길동" });
+app.use(router);
+app.mount("#app");
 
 import "bootstrap/dist/js/bootstrap.js";
 
 // 어떤 모드인지 출력
-console.log("MODE:", import.meta.env.MODE);
-console.log("MODE:", import.meta.env.BASE_URL);
+// console.log("MODE:", import.meta.env.MODE);
+// console.log("MODE:", import.meta.env.BASE_URL);
 // Production모드이면 true
-console.log("MODE:", import.meta.env.PROD);
+// console.log("MODE:", import.meta.env.PROD);
 // Dev 모드면 true
-console.log("MODE:", import.meta.env.DEV);
+// console.log("MODE:", import.meta.env.DEV);
 
-console.log("VITE_APP_API_URL", import.meta.env.VITE_APP_API_URL);
+// console.log("VITE_APP_API_URL", import.meta.env.VITE_APP_API_URL);
