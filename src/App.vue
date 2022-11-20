8c541ba671f45e45acb1b48a1ece4ac29bc4310e
leegyuseung <lgs4002@naver.com>
2022-11-20 21:54:49 +0900

    composable : Alert 컴포저블 함수

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <script setup>
 import TheHeader from "@/layouts/TheHeader.vue";
 import TheView from "@/layouts/TheView.vue";
+import { useAlert } from "./composables/alert";
+const { alerts } = useAlert();
 </script>
 
-<template><TheHeader /><TheView /></template>
+<template><TheHeader /><TheView /> <AppAlert :items="alerts" /></template>
 
 <style scoped></style>
--- src/views/posts/PostEditView.vue
+++ src/views/posts/PostEditView.vue
@@ -19,7 +19,6 @@
       </template>
     </PostForm>
     <!-- <AppAlert :show="showAlert" :message="alertMessage" :type="alertType" /> -->
-    <AppAlert :items="alerts" />
   </div>
 </template>
 
@@ -28,7 +27,9 @@ import { getPostById, updatePost } from "@/api/posts";
 import { ref } from "vue";
 import { useRoute, useRouter } from "vue-router";
 import PostForm from "@/components/posts/PostForm.vue";
+import { useAlert } from "@/composables/alert";
 
+const { vAlert, vSuccess } = useAlert();
 const route = useRoute();
 const router = useRouter();
 const id = route.params.id;
@@ -55,8 +56,8 @@ fetchPost();
 const edit = async () => {
   try {
     await updatePost(id, { ...form.value });
-    // router.push({ name: "PostDetail", params: { id } });
-    vAlert("수정이 완료되었습니다.", "success");
+    router.push({ name: "PostDetail", params: { id } });
+    vSuccess("수정이 완료되었습니다.");
   } catch (error) {
     console.error(error);
     vAlert(error.message);
@@ -64,22 +65,6 @@ const edit = async () => {
 };
 
 const goDetailPage = () => router.push({ name: "PostDetail", params: { id } });
-
-// alert
-// const showAlert = ref(false);
-// const alertMessage = ref("");
-// const alertType = ref("");
-const alerts = ref([]);
-const vAlert = (message, type = "error") => {
-  alerts.value.push({ message, type });
-  // showAlert.value = true;
-  // alertMessage.value = message;
-  // alertType.value = type;
-  setTimeout(() => {
-    // showAlert.value = false;
-    alerts.value.shift();
-  }, 2000);
-};
 </script>
 
 <style lang="scss" scoped></style>
