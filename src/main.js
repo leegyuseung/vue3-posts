4901104eaa7e68c992a71658d10699270ba9ae1a
leegyuseung <lgs4002@naver.com>
2022-11-26 23:58:58 +0900

    pinia 기초:state,getters,actions

--- src/main.js
+++ src/main.js
@@ -3,32 +3,15 @@ import "bootstrap-icons/font/bootstrap-icons.css";
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "@/router";
-// import funcPlugins from "./plugins/func";
-// import objPlugins from "./plugins/obj";
-// import person from "./plugins/person";
-// import globalComponents from "@/plugins/global-components";
-// import focus from "./directives/focus";
 import globalDirectives from "./plugins/global-directives";
 import dayjs from "./plugins/dayjs";
+import { createPinia } from "pinia";
 
 const app = createApp(App);
-// app.use(globalComponents);
-// app.directive("focus", focus);
+
 app.use(router);
 app.use(globalDirectives);
 app.use(dayjs);
-// app.use(objPlugins, { name: "lee" });
-// app.use(funcPlugins);
-// app.use(person, { name: "홍길동" });
+app.use(createPinia());
 app.mount("#app");
 import "bootstrap/dist/js/bootstrap.js";
-
-// 어떤 모드인지 출력
-// console.log("MODE:", import.meta.env.MODE);
-// console.log("MODE:", import.meta.env.BASE_URL);
-// Production모드이면 true
-// console.log("MODE:", import.meta.env.PROD);
-// Dev 모드면 true
-// console.log("MODE:", import.meta.env.DEV);
-
-// console.log("VITE_APP_API_URL", import.meta.env.VITE_APP_API_URL);
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -5,14 +5,30 @@
     <button class="btn btn-primary" @click="$router.push('/')">
       Home으로 이동
     </button>
+    <h2>Store</h2>
+    <p>counter:{{ counter }}</p>
+    <p>doubleCounter:{{ doubleCounter }}</p>
+    <p>doubleCounterPlusOne:{{ doubleCounterPlusOne }}</p>
+    <button @click="increment()">Click</button>
   </div>
 </template>
 
 <script setup>
 import { useRoute } from "vue-router";
+import { useCounterStore } from "@/stores/counter";
+import { storeToRefs } from "pinia";
 
 const route = useRoute();
 console.log("route.path:", route.path);
+
+const store = useCounterStore();
+console.log(store);
+
+// 반응성을 유지
+const { counter, doubleCounter, doubleCounterPlusOne } = storeToRefs(store);
+const { increment } = store;
+// counter.value = 100;
+// increment();
 </script>
 
 <style lang="scss" scoped></style>
